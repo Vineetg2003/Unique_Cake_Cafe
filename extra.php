4ca5bb09fb0e4ef95ae41c961857ba56ad83e74a
Vineetg2003 <vineetgoyal130@gmail.com>
2023-12-21 16:58:10 +0530

    added

--- extra.php
+++ extra.php
@@ -1,4 +1,22 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$mobile= $_POST['mobile'];
+$address= $_POST['Address'];
+$to = "vineetgoyal130@gmail.com";
 
+$subject = "Mail From Unique Cake Cafe";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message."\r\n Mobile Number = ". $mobile ."\r\n Address = " .$address;
+$headers = "From: noreply@uniquecakecafe.com" . "\r\n" .
+// "CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
 <?php
 //get data from form  
 $name = htmlspecialchars($_POST['name']);
